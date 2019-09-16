a3c53706bb356f8dd32cf3412859ff3f48c7c483
Daniel Gagliardi <daniel.gagliardi216@gmail.com>
2019-09-16 18:45:05 +0100

    added stuff

--- sketch.js
+++ sketch.js
@@ -11,7 +11,13 @@ function draw() {
   background(0);
   translate(w/2, h/2);
   let radius = h/6;
+
   stroke(255);
+  noFill();
   ellipse(0, 0, radius * 2);
+
+  let x = radius * cos(time);
+  let y = radius * sin(time);
+  creata
   time += 0.01;
 }
